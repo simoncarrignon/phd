06930ca0e5f65ee419e11aae2b28478e4f9b8966
Simon <simon.carrignon@gmail.com>
2015-09-08 12:59:45 +0200

    philoscript

---
+++ dev/philo/getSotonDB.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import urllib2
+from HTMLParser import HTMLParser
+import csv
+
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print "Encountered a start tag:", 
+    def handle_endtag(self, tag):
+        print "Encountered an end tag :", tag
+    def handle_data(self, data):
+        print "Encountered some data  :", data
+
+baseurl = "http://archaeologydataservice.ac.uk/archives/view/amphora_ahrb_2005/character.cfm?id="
+parser = MyHTMLParser()
+
+for i in range(10):
+    url = baseurl + str(i)
+    request = urllib2.Request(url)
+    request.add_header('User-Agent','Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)')
+    request.add_header('Content-Type','application/json')
+    response = urllib2.urlopen(request)
+    soup = BeautifulSoup(response.read(), 'html.parser')
+    for t in  soup.body.find_all('td', attrs={'class':'dets'}):
+        print t.find('a').text
+    
