269f6f3e2e5d4ba42a8122225aed8833e2fcf078
Simon <simon.carrignon@gmail.com>
2016-02-05 14:44:41 +0100

    [MEDEA MNSTRUM] confile err

--- dev/medeaNetwork/allSizeTour.sh
+++ dev/medeaNetwork/allSizeTour.sh
@@ -5,6 +5,6 @@ for ts in `seq 2 1 8`;
 do
 	sed -i "s/gTournamentSize = [0-9][0-9]*/gTournamentSize = $ts/" $confFile
 	echo "-- t_size at $ts%"
-	perso/simon/sparsity/testAllSparsity.sh $conFile 200 6 900 980 1 
+	perso/simon/sparsity/testAllSparsity.sh $confFile 200 6 900 980 1 
 done
 	
