9717a905e22bdef482f14c4951650a28d164711a
Simon <simon.carrignon@gmail.com>
2016-02-05 14:19:35 +0100

    [MEDEA MNSTRUM] some scripting changes

--- dev/medeaNetwork/allSizeTour.sh
+++ dev/medeaNetwork/allSizeTour.sh
@@ -1,9 +1,10 @@
 
+confFile=$1
 
-for ts in `seq 1 1 10`;
+for ts in `seq 2 1 8`;
 do
-	sed -i "s/gTournamentSize = [0-9][0-9]*/gTournamentSize = $ts/" prj/mEDEA-sp/propertieFiles/sparsity.properties
+	sed -i "s/gTournamentSize = [0-9][0-9]*/gTournamentSize = $ts/" $confFile
 	echo "-- t_size at $ts%"
-	perso/simon/sparsity/testAllSparsity.sh 200 6 900 980 1 
+	perso/simon/sparsity/testAllSparsity.sh $conFile 200 6 900 980 1 
 done
 	
