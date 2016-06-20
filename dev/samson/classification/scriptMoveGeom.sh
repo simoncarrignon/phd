f9f06b2d80aec6a22b9fd8bf27c11ed0335a80c4
Simon Carrignon <simon.carrignon@gmail.com>
2016-06-20 21:35:51 +0200

    update of some dev submodule

--- dev/samson/classification/scriptMoveGeom.sh
+++ dev/samson/classification/scriptMoveGeom.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #Script to cehck if theyre are classified as geom and move the file
-cat $1  | awk 'BEGIN{FS=";"}{if($5>1904) print $14}'  | grep pdf | sed 's/pdf/txt/' > unlab #tex newer than 1904
-cat $1  | awk 'BEGIN{FS=";"}{if($15!="" && $5<=1904) print $14}'  | grep pdf | sed 's/pdf/txt/'  > geom
+cat $1  | awk 'BEGIN{FS=";"}{if($5>1904 && !($2 ~ /eomet/) ) print $14}'  | grep pdf | sed 's/pdf/txt/' > unlab #tex newer than 1904
+cat $1  | awk 'BEGIN{FS=";"}{if($5>1904 && ($2 ~ /eomet/) ) print $14}'  | grep pdf | sed 's/pdf/txt/' > geom #tex newer than 1904
+#cat $1  | awk 'BEGIN{FS=";"}{if($5>1904) print $14}'  | grep pdf | sed 's/pdf/txt/' > unlab #tex newer than 1904
+cat $1  | awk 'BEGIN{FS=";"}{if($15!="" && $5<=1904) print $14}'  | grep pdf | sed 's/pdf/txt/'  >> geom
 cat $1  | awk 'BEGIN{FS=";"}{if($15=="" && $5<=1904) print $14}'  | grep pdf | sed 's/pdf/txt/'  > nongeom
 
 
